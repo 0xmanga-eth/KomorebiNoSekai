9d3f7fe26fe05d1e1916f721c26152caa44e7c26
0xmanga-eth <0xmanga.crypto@gmail.com>
2022-03-14 18:55:40 +0100

    add flipper failing test to investigate revert issue

--- test/komorebi/KomorebiFlipper.behavior.ts
+++ test/komorebi/KomorebiFlipper.behavior.ts
@@ -2,5 +2,9 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 export function shouldBehaveLikeKomorebiFlipper(): void {
-  it("flips", async function () {});
+  it("flips", async function () {
+    await this.komorebiNoSekai.connect(this.signers.admin).devMint(10);
+    await this.komorebiFlipper.connect(this.signers.admin).addInPool([0]);
+    //await this.komorebiFlipper.connect(this.signers.anyone).play(0, false);
+  });
 }
